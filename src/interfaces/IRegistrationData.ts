e52ee41596c50a646ffd4fc4b3a769b3607392d1
Samir Shrestha <linkwithsamir@gmail.com>
2024-01-14 08:50:02 +0545

    add: gender enum

--- src/interfaces/IRegistrationData.ts
+++ src/interfaces/IRegistrationData.ts
@@ -2,10 +2,15 @@ import { IBase } from './IBase';
 
 export interface IRegistrationData extends IBase {
   fullname: string;
-  gender: string;
+  gender: Gender;
   age: number;
   address: string;
   phoneNumber: string;
   email: string;
   password: string;
+}
+
+enum Gender {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
 }
